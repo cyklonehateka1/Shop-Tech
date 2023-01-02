de96cecb69830f7b74f17aa8a98601b57d57bb88
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-01-02 16:37:18 +0000

    Done with categories component design for now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../components/Categories";
 import FilterList from "../components/FilterList";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -13,6 +14,7 @@ const Home = () => {
       <Navbar />
       <div className="homeMainCont">
         <Hero />
+        <Categories />
         <FilterList />
         <Products />
       </div>
