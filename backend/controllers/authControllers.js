40ed840b7b753da470916f92abf241e32149a221
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-01-08 18:33:04 +0000

    Integrating register auth into client

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,11 +1,17 @@
 import UserSchema from "../models/User.js";
 import { errorHandler } from "../middlewares/errorHandler.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const register = async (req, res, next) => {
+export const register = async (req, res, next) => {
   let { name, email, password } = req.body;
   name = name.trim();
   password = password.trim();
   email = email;
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
   if (!name || !email || password)
     return next(errorHandler(400, "All fields are requried"));
@@ -18,8 +24,44 @@ const register = async (req, res, next) => {
           "An account with this email already exist, will you like to login instead"
         )
       );
-    user = new UserSchema({ ...req.body });
+    user = new UserSchema({ ...req.body, password: hash });
     await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.EMAIL_CON_KEY, {
+      expiresIn: "1h",
+    });
+    const url = `${process.env.BASE_URL}/api/auth/${user._id}/verify/${token}`;
+
     res.status(201).json("Please click on the link in the email sent to you");
   } catch (error) {}
+};
+
+export const confirmAccount = async (req, res, next) => {
+  try {
+    const user = await UserSchema.findById(req.params.userId);
+    if (!user) return next(errorHandler(401, "Invalid user"));
+
+    const verifyToken = jwt.verify(
+      req.params.token,
+      process.env.EMAIL_CON_KEY,
+      (error, response) => {
+        if (error) return next(errorHandler(401, "Token has expired"));
+
+        if (response.id !== req.params.partnerId)
+          return next(errorHandler(403, "Invalid token"));
+      }
+    );
+
+    const verify = await PartnerSchema.findByIdAndUpdate(
+      req.params.partnerId,
+      {
+        $set: { emailVerified: true },
+      },
+      { new: true }
+    );
+
+    res.status(200).json("Email verified successfully");
+  } catch (error) {
+    return next(error);
+  }
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -36,3 +36,5 @@ const UserSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+export default mongoose.model("user", UserSchema);
