8152bcf5b26063082a962f294284b8919baf1bbf
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-11-04 05:30:26 +0000

    logout takes you to login page

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({ origin: "https://shop-tech-iota.vercel.app", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
