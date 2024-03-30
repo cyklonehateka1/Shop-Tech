a7b6ca0f9d2e67f47e6d1e0ea9eefea5421a4a0e
EMMANUEL HATEKA <84219325+cyklonehateka1@users.noreply.github.com>
2024-03-30 11:26:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({ origin: "https://shop-tech-iota.vercel.app", credentials: true })
-  // cors({ origin: "http://localhost:3000", credentials: true })
+  cors({ origin: "http://localhost:5173", credentials: true })
 );
 app.use(express.json());
 app.use(cookieParser());
